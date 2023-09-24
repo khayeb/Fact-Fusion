ceff46bff429a06fc92c23393b845bbe0066d997
khayeb <buerano.khaye@ue.edu.ph>
2023-09-24 23:04:35 +0800

    add files

---
+++ app/src/main/java/com/example/factfusion/QuestionAnswer.java
@@ -0,0 +1,27 @@
+package com.example.factfusion;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "Which is the only sport to have been played on the moon?",
+            "Which country means 'fragrant harbor'?",
+            "What color is the blood of an octopus?",
+            "Who owns all the England's swans?",
+            "Where were French Fries first made?"
+
+    };
+    public static String choices[][] = {
+            {"Baseball","Fencing","Golf","Cycling"},
+            {"Hong Kong","Taiwan","China","Malaysia"},
+            {"Pink","Blue","White","Red"},
+            {"The Queen","The Prince","The Dutch","The King"},
+            {"Spain","France","USA","Belgium"}
+    };
+    public static String correctAnswers[] = {
+            "Golf",
+            "Hong Kong",
+            "Blue",
+            "The Queen",
+            "Belgium"
+    };
+}
